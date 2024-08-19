2ec8f84732922702ac13bc5f2b891ad7977d51d3
Albert Harvey Garcia <95603-18530560@SERVER2.DOMAIN>
2024-08-19 18:29:23 +0800

    first commit

---
+++ sample.js
@@ -0,0 +1,17 @@
+const user = {
+    fname: "Albert",
+    lname: "Garcia",
+    gender: "Male",
+    age: 25
+}
+const fname = user.fname
+const lname = user.lname
+const gender = user.gender
+const age = user.age
+
+
+ 
+ console.log("Me"+ " " + fname + " " + lname +" " + age + "years old")
+
+
+
